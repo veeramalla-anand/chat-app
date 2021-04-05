a4193d4df055f61f85ae1910ff0477af63b7c528
veeramalla-anand <anandveeramalla202@gmail.com>
2021-04-05 22:20:28 +0530

    final

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ app.get ('/',(req,res)=>{
 });
 io.on('connection',(socket)=>{
     console.log("a user is connected");
+    socket.on('chat message',(msg)=>{
+        io.emit('chat message',msg);
+    })
+    socket.on('disconnect',()=>{
+        console.log("user disconnected")
+    })
 });
 http.listen(3000,()=>{
     console.log('listening on port 3000')
